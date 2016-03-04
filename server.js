a13f464c9cf118cedca548bbeb2dba64cd00f7fd
unknown <ducquang310@yahoo.com>
2016-03-04 10:46:30 +0700

    Quang

--- server.js
+++ server.js
@@ -1,2 +1,6 @@
-Testing
-Nguyen Duc Quang
+var http = require('http');
+http.createServer(function(request, response) {
+	response.writeHeader('Content-Type', 'text/plain');
+	response.write('Hello world');
+	response.end();
+}).listen(5000);
